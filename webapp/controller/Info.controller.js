06568631c9abaf726994fb28d3732fed44e71e37
Mauricio Alcala <mauricio.alcala@softtek.com>
2019-05-23 15:06:58 +0000

    datail_view_implemented

--- webapp/controller/Info.controller.js
+++ webapp/controller/Info.controller.js
@@ -3,13 +3,32 @@ sap.ui.define([
 ], function (BaseController) {
 	"use strict";
 
-	return BaseController.extend("opensap.orders.Orders.controller.Info", {	
+	return BaseController.extend("opensap.orders.Orders.controller.Info", {
 		onInit: function () {
 			this.getRouter().getRoute("Info").attachPatternMatched(this._onInfoMatched, this);
 		},
-		_onInfoMatched: function(oEvent)
-		{
-			this.getModel("appView").setProperty("/layout", "ThreeColumnsMidExpanded");
+		/**
+		 * Binds the view to the object path and expands the aggregated line items.
+		 * @function
+		 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
+		 * @private
+		 */
+		_onInfoMatched: function (oEvent) {
+			var sObjectId = oEvent.getParameter("arguments").objectId,
+				sItemPosition = oEvent.getParameter("arguments").itemPosition;
+			this.getModel("appView").setProperty("/layout", "ThreeColumnsEndExpanded");
+			this.getModel().metadataLoaded().then(function () {
+				var sObjectPath = this.getModel().createKey("SalesOrderLineItemSet", {
+					SalesOrderID: sObjectId,
+					ItemPosition: sItemPosition
+				});
+				this.getView().bindElement({
+					path: "/" + sObjectPath,
+					parameters: {
+						'expand': 'ToHeader'
+					}
+				});
+			}.bind(this));
 		}
 	});
 
