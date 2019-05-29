831551918c50320fb450724f044fae3e74d3e514
Mauricio Alcala <mauricio.alcala@softtek.com>
2019-05-29 15:03:32 +0000

    saving

--- webapp/controller/Create.controller.js
+++ webapp/controller/Create.controller.js
@@ -1,17 +1,39 @@
 sap.ui.define([
-	"./BaseController"
-], function (BaseController) {
+	"./BaseController",
+	"sap/m/MessageToast"
+], function (BaseController, MessageToast) {
 	"use strict";
-	return BaseController.extend("opensap.orders.Orders.controller.Create", {
+
+	return BaseController.extend("opensap.orders.Ord.controller.Create", {
+
 		onInit: function () {
 			// create a message manager and register the message model
 			this._oMessageManager = sap.ui.getCore().getMessageManager();
 			this._oMessageManager.registerObject(this.getView(), true);
 			this.setModel(this._oMessageManager.getMessageModel(), "message");
-			this.getRouter().getRoute("create").attachPatternMatched(this._onCreateMatched,
-				this);
+
+			this.getRouter().getRoute("create").attachPatternMatched(this._onCreateMatched, this);
 		},
+
 		_onCreateMatched: function (oEvent) {
+			var sObjectId = oEvent.getParameter("arguments").objectId;
+
+			// create a binding context for a new order item
+			this.oContext = this.getModel().createEntry("/SalesOrderLineItemSet", {
+				properties: {
+					SalesOrderID: sObjectId,
+					ProductID: "",
+					Note: "",
+					Quantity: "1",
+					DeliveryDate: new Date()
+				},
+				success: this._onCreateSuccess.bind(this)
+			});
+			this.getView().setBindingContext(this.oContext);
+
+			// reset potential server-side messages
+			this._oMessageManager.removeAllMessages();
+
 			// set a dynamic date constraint in controller, as "today" cannot be
 			// defined declaratively in XMLView
 			var oToday = new Date();
@@ -19,17 +41,54 @@ sap.ui.define([
 			this.byId("deliveryDate").getBinding("value").getType().setConstraints({
 				minimum: oToday
 			});
+
 			this.getModel("appView").setProperty("/layout", "ThreeColumnsMidExpanded");
 		},
+
+		_onCreateSuccess: function (oContext) {
+			// show success message
+			var sMessage = this.getResourceBundle().getText("newItemCreated", [oContext.ProductID]);
+			MessageToast.show(sMessage, {
+				closeOnBrowserNavigation : false
+			});
+
+			// navigate to the new item in display mode
+			this.getRouter().navTo("Info", {
+				objectId : oContext.SalesOrderID,
+				itemPosition : oContext.ItemPosition
+			}, true);
+		},
+
+		onCreate: function () {
+			// send new item to server for processing
+			this.getModel().submitChanges();
+		},
+
 		onCancel: function () {
 			var sObjectId = this.getView().getBindingContext().getProperty("SalesOrderID");
+
 			// discard the new context on cancel
 			this.getModel().deleteCreatedEntry(this.oContext);
+
 			// close the third column
 			this.getRouter().navTo("object", {
-				objectId: sObjectId
+				objectId : sObjectId
 			}, true);
+		},
+
+		onNameChange: function () {
+			// clear potential server-side messages to allow saving the item again
+			this._oMessageManager.getMessageModel().getData().forEach(function(oMessage){
+				if (oMessage.code) {
+					this._oMessageManager.removeMessages(oMessage);
+				}
+			}.bind(this));
+		},
+
+		onOpenMessages: function (oEvent) {
+			this.byId("messages").openBy(oEvent.getSource());
 		}
 
 	});
+
 });
