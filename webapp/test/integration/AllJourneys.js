a9c6483a56372a563537b84460de1272c0f79c96
Mauricio Alcala <mauricio.alcala@softtek.com>
2019-05-30 15:09:03 +0000

    test_done

--- webapp/test/integration/AllJourneys.js
+++ webapp/test/integration/AllJourneys.js
@@ -6,10 +6,7 @@
 sap.ui.define([
 	"sap/ui/test/Opa5",
 	"./arrangements/Startup",
-	"./MasterJourney",
-	"./NavigationJourney",
-	"./NotFoundJourney",
-	"./BusyJourney"
+	"./NavigationJourney"
 ], function (Opa5, Startup) {
 	"use strict";
 	Opa5.extendConfig({
